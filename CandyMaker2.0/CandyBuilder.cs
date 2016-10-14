ccb0614127aa64c4be4f2dae59d39f7b8431931c
Hexanix <damian.d@abv.bg>
2016-10-14 18:38:56 +0300

    Starting point
    This containts jsut the basic build class with two WriteLine commands
    and passed variables;

---
+++ CandyMaker2.0/CandyBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyMaker2._0
+{
+    class CandyBuilder
+    {
+        public void CandyBuild(string CandyColor, string CandyShape, string CandyMaterial)
+        {
+            Console.WriteLine("This is a " + CandyColor + CandyShape + " candy made out of " + CandyMaterial + ".");
+        }
+
+
+        public void WrapBuild(string WrapColor, string WrapMaterial)
+        {
+            Console.WriteLine("It is wrapped in a " + WrapColor + " wrapping made out of " + WrapMaterial);
+        }
+
+    }
+}
