8ce80079e3a65576797db380e90dccbc009754d8
Hexanix <damian.d@abv.bg>
2016-10-17 09:46:34 +0300

    This is the first semi-working test
    There are still a fe bugs
    it doesn't print out the candy-specific strings
    It also doesnt properly random-generate the indexes(Always the same)

---
+++ CandyMaker2.0/CandyAbstractData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyMaker2._0
+{
+    abstract class CandyAbstractData
+    {
+        //These are abstracts that are the basis of all candy
+        public abstract string CandyType { get; set; }
+        public abstract string CandyShape { get; set; }
+        public abstract string CandyMaterial { get; set; }
+        public abstract string CandyTaste { get; set; }
+
+        //these are variables to hold the optional values for the candy
+        protected string candyColor { get; set; }
+        protected string candyAroma { get; set; }
+        protected int candyWeight { get; set; }
+    }
+}
+
+
+
+    
+
